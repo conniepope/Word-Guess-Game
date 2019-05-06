73c9327d29bdd9dce974b0abd768a5cb8192a6c0
conniepope <conniepope77@gmail.com>
2019-05-05 21:47:00 -0400

    html setup & pseudo-code

---
+++ assets/javascript/game.js
@@ -0,0 +1,71 @@
+
+
+/*   Use key events to listen for the letters that your players will type.  */
+
+document.onkeyup = function (event) {
+
+}
+
+/*    Wins: (# of times user guessed the word correctly). */
+
+if (keyPress  === v || e || n) {
+    /* write the letter in its appropriate place */
+} else {
+    /* write the letter in letters guessed */
+
+}
+
+/*   * If the word is `madonna`, display it like this when the game starts: `_ _ _ _ _ _ _`. */
+if (currentWord === words.venezuela) {
+    /* write _ _ _ _ _ _ _ _ _  */
+}
+ /*   * As the user guesses the correct letters, reveal them: `m a d o _  _ a`.
+
+7. Number of Guesses Remaining: (# of guesses remaining for the user).
+
+
+8. Letters Already Guessed: (Letters the user has guessed, displayed like `L Z Y H`). */
+
+if (numGuessRemain === 0) {
+    losses++
+}
+/* 9. After the user wins/loses the game should automatically choose another word and make the user play it. 
+
+
+4. Save your whole game and its properties in an object.
+5. Save any of your game's functions as methods, and call them underneath your object declaration using event listeners.
+6. Don't forget to place your global variables and functions above your object.
+   * Remember: global variables, then objects, then calls.   */
+
+
+
+/*  VARIABLES  */
+ var keyPress
+ var wins
+ var losses
+ var currentWord
+ var numGuessRemain
+ var lettersGuessed
+
+
+/*  OBJECTS  */
+var currentWord = {
+    venezuela: [v, e, n, e, z, u, e, l, a],
+    mozambique: [m, o, z, a, m, b, i, q, u, e],
+}
+
+
+
+/*  CALLS  */
+
+
+
+/*   words for game:
+VENEZUELA
+MOZAMBIQUE
+MADAGASCAR
+PORTUGAL
+AUSTRALIA
+KAZAKHSTAN
+CAMEROON   */
+
