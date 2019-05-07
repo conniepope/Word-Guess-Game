1b28486c911fdeb03c5b99dc0579f8053c6338e4
conniepope <conniepope77@gmail.com>
2019-05-07 09:30:41 -0400

    took out objects, added arrays, built one MAIN function.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,12 +9,16 @@ Don't forget to place your global variables and functions above your object. */
 
 
 /*  VARIABLES  */
+ var keyPress
  var wins = 0
  var losses = 0
- var currentWordIndex = 0
- var currentWord = ["venezuela", "mozambique", "madagascar"]
+ var currentWord = ["venezuela", "mozambique", "madagascar", "portugal", "australia", "kazakhstan", "cameroon"]
  var numGuessRemain
  var lettersGuessed
+ var word = currentWord[Math.floor(Math.random() * currentWord.length)];
+
+
+
 
  var winsText = document.getElementById("winsText");
  var lossesText = document.getElementById("lossesText");
@@ -24,64 +28,42 @@ Don't forget to place your global variables and functions above your object. */
 
 
 
-/*  OBJECTS (& methods)  */
-
-var venezuela = {
-    letters: [v, e, n, e, z, u, e, l, a],
-    guesses: 9,
-    runVenezuela: function() {
-
-        currentWordText = "_ _ _ _ _ _ _ _ _";
-
-        /* how to replace underscores with proper letter when pushed?. Push to an index #?    */
-        
-        for (var i = 0; i < letters.length; i++) {
-            if (numGuessRemain === 0) {
-                losses++
-            };
-
-            if (keyPress  === v || e || n || z ||u || l || a) {
-            return venezuela.findIndex
-            }
-
-            if (keyPress.indexOf(venezuela) === 0) {
-            currentWordText.innerHTML = "V"
-            } else if 
-                (keyPress.indexOf(venezuela) === 1 && 3 && 6)  {
-            currentWordText.innerHTML = "E"
-
-            } else {
-            /* write the letter in letters guessed:   */
-            (lettersGuessed.push = keyPress) && numGuessRemain--
-            }
-        }
-    }
-/* How to end word round and start new one? */
-
-
-};
-var mozambique = {
-    letters: [m, o, z, a, m, b, i, q, u, e],
-    guesses: 10,
-};
-
-
 /*  MAIN FUNCTIONS  */
-
-/* auto-select a word from the currentWord array, then run the function that applies. */
-for (var i = 0; i < currentWord.length; i++) {
-    currentWord[i]
-}
-/* make the # of spaces = the # of letters in the word, and # of guesses remaining to be set as # of guesses set for that word. */    
- 
-if (currentWord[i] === venezuela) {
-    venezuela.runVenezuela();
-    /* write _ _ _ _ _ _ _ _ _          .toUpperCase    */
-}
 
 /*   Use key events to listen for the letters that your players will type.  */
 document.onkeyup = function (event) {
     keyPress = event.key;
+}        
+
+var answer = [];
+for (var i = 0; i < currentWord.length; i++) {
+     answer[i] = "_";
+    
+    if (numGuessRemain === 0) {
+        losses++;
+    }; 
+
+    while (numGuessRemain > 0) {
+        for (var i = 0; i < currentWord.length; i++) {
+            if (currentWord[i] === keyPress && !"_") {
+                answer[i] = keyPress;
+                numGuessRemain--;
+            }
+        }
+    }
+}
+
+/* How to end word round and start new one? */
+
+
+
+/* auto-select a word from the currentWord array, then run the function that applies. */
+
+
+ 
+if (currentWord[i] === venezuela) {
+    venezuela.runVenezuela();
+    /* write _ _ _ _ _ _ _ _ _          .toUpperCase    */
 }
 
  /* Determine if win or loss */
@@ -91,7 +73,6 @@ document.onkeyup = function (event) {
 
 /*  CALLS  */
 
-
 winsText.textContent = "Wins: " + wins;
 lossesText.textContent = "Losses: " + losses;
 currentWordText.textContent = "";
@@ -99,12 +80,4 @@ numGuessRemainText.textContent = "Number of Guesses Remaining: " + i--;
 lettersGuessedText.textContent = "Letters Already Guessed: " + lettersGuessed;
 
 
-/*   words for game:
-VENEZUELA
-MOZAMBIQUE
-MADAGASCAR
-PORTUGAL
-AUSTRALIA
-KAZAKHSTAN
-CAMEROON   */
 
