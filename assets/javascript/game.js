62bce7f3f3e1da127866ffec8399c720b469ef3f
conniepope <conniepope77@gmail.com>
2019-05-07 13:30:23 -0400

    JS code wrote & organized. Currently does not work.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,83 +1,100 @@
 
-    
- /* 
-Save any of your game's functions as methods, and call them underneath your object declaration using
-EVENT LISTENERS.
-
-Don't forget to place your global variables and functions above your object. */
-
-
 
 /*  VARIABLES  */
- var keyPress
- var wins = 0
- var losses = 0
- var currentWord = ["venezuela", "mozambique", "madagascar", "portugal", "australia", "kazakhstan", "cameroon"]
- var numGuessRemain
- var lettersGuessed
- var word = currentWord[Math.floor(Math.random() * currentWord.length)];
-
-
-
+ var wins = 0;
+ var losses = 0;
+ var currentWord = ["venezuela", "mozambique", "madagascar", "portugal", "australia", "kazakhstan", "cameroon"];
 
  var winsText = document.getElementById("winsText");
  var lossesText = document.getElementById("lossesText");
- var currentWordText = document.getElementById("currentWordText");
- var numGuessRemainText = document.getElementById("numGuessRemainText");
- var lettersGuessedText = document.getElementById("lettersGuessedText");
 
+ /* currentWordText needs connected in the code */
+ var currentWordText = document.getElementById("currentWordText");
+ var numGuessRemain = document.getElementById("numGuessRemainText");
+ var lettersGuessed = document.getElementById("lettersGuessedText");
 
 
 /*  MAIN FUNCTIONS  */
 
-/*   Use key events to listen for the letters that your players will type.  */
+/*  key events to listen for the letters that players will type. FIND OUT HOW THIS WORKS AND PROPER PLACEMENT FOR IT. */
 document.onkeyup = function (event) {
-    keyPress = event.key;
-}        
+    var keyPress = event.key;
+               
+    /* picks random word */
+    var word = currentWord[Math.floor(Math.random() * currentWord.length)]; 
 
-var answer = [];
-for (var i = 0; i < currentWord.length; i++) {
-     answer[i] = "_";
-    
-    if (numGuessRemain === 0) {
-        losses++;
-    }; 
+    /* answer array - setup */
+    var answer = [];
+    for (var i = 0; i < word.length; i++) {
+        answer[i] = "_";
+    }
+    var numGuessRemain = word.length;
+
+/* game loop */
 
     while (numGuessRemain > 0) {
-        for (var i = 0; i < currentWord.length; i++) {
-            if (currentWord[i] === keyPress && !"_") {
+            
+        /* to make letters appear in place of underscores */
+
+        for (var i = 0; i < word.length; i++) {
+            if (word[i] === keyPress) {
                 answer[i] = keyPress;
                 numGuessRemain--;
             }
+        /* if not a letter in the word */
+            else if (word[i] !== keyPress) {
+                (lettersGuessed + " "  + keyPress);
+                numGuessRemain--;
+            }
+
+        /* if letter already pressed, do not take away guesses    */
+            else if (keyPress === letterGuessed) {
+                    numGuessRemain + 0;
+            }
         }
+
+        /* Determine if win or loss. Automatically choose another word to be played. */
+
+        if (numGuessRemain === 0 && answer === word) {
+            wins++;
+        } else {
+            losses++;
+        }; 
     }
-}
-
-/* How to end word round and start new one? */
-
-
-
-/* auto-select a word from the currentWord array, then run the function that applies. */
-
-
- 
-if (currentWord[i] === venezuela) {
-    venezuela.runVenezuela();
-    /* write _ _ _ _ _ _ _ _ _          .toUpperCase    */
-}
-
- /* Determine if win or loss */
-
-/* After the user wins/loses, the game should automatically choose another word and make the user play it. 
+}       
 
 
 /*  CALLS  */
 
-winsText.textContent = "Wins: " + wins;
+
+/* coming up in console as "null"  ????*/
+winsText.textContent = "Wins: " + wins;    
 lossesText.textContent = "Losses: " + losses;
-currentWordText.textContent = "";
-numGuessRemainText.textContent = "Number of Guesses Remaining: " + i--;
-lettersGuessedText.textContent = "Letters Already Guessed: " + lettersGuessed;
+numGuessRemainText.textContent = "Number of Guesses Remaining: " + numGuessRemain;
+lettersGuessedText.textContent = "Letters Already Guessed: " + lettersGuessed.toUpperCase;
+
+
+
+
+/* OTHER IDEAS */
+
+/*   
+
+currentWordText.textContent = answer.join(" ");   
+
+innerHTML instead of textContent ???
+
+auto-select a word from the currentWord array, then run the function that applies. 
+
+object with method????
+    if (currentWord[i] === venezuela) {
+        venezuela.runVenezuela();
+    
+
+*/
+
+
+
 
 
 
